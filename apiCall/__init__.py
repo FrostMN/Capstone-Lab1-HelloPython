769961e6db6c581d2bec812fb96fa61bdceffbf8
aaron <asouer@gmail.com>
2018-01-09 16:07:19 -0600

    moved key to env var

--- apiCall/__init__.py
+++ apiCall/__init__.py
@@ -1,17 +1,17 @@
 import requests
 import json
-
-api_key = ""
+import os
+api_key = os.environ["ZIP_KEY"]
+del os
 api_url = "https://www.zipcodeapi.com/rest/<api_key>/distance.json/<zip_code1>/<zip_code2>/mile"
 api_url = api_url.replace("<api_key>", api_key)
 
 
 if __name__ == '__main__':
-    # first_zip = input("please enter the first zipcode: ")
-    # second_zip = input("please enter the second zipcode: ")
-    first_zip = str(55414)
-    second_zip = str(55418)
-
+    first_zip = input("please enter the first zipcode: ")
+    second_zip = input("please enter the second zipcode: ")
+    # first_zip = str(55414)
+    # second_zip = str(55418)
 
     api_url = api_url.replace("<zip_code1>", first_zip)
     api_url = api_url.replace("<zip_code2>", second_zip)
